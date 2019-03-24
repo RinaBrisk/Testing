f323ca63394f8efbc465c4aa1813acadcc1c3511
Rina <rina_sergeeva@bk.ru>
2019-03-24 20:42:19 +0300

    created run from command line

--- src/test/java/BaseRunner.java
+++ src/test/java/BaseRunner.java
@@ -1,26 +1,44 @@
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseRunner {
 
+    private static ThreadLocal<WebDriver> threadLocal = new ThreadLocal<>();
+    private String browserName = System.getProperty("browser");
     WebDriver driver;
     String baseUrl;
 
     @Before
     public void setUp(){
-        driver = BrowsersFactory.firefox.create();
-       // driver = BrowsersFactory.chrome.create();
+        if (threadLocal.get() != null) {
+            driver = threadLocal.get();
+        } else {
+            driver = getDriver();
+            threadLocal.set(driver);
+        }
         driver.manage().window().maximize();
         baseUrl = "https://www.tinkoff.ru/career/vacancies/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            driver.quit();
+            driver = null;
+        }));
+    }
+
+    private WebDriver getDriver() {
+        try {
+            BrowsersFactory.valueOf(System.getProperty("browser"));
+        } catch (NullPointerException | IllegalArgumentException e) {
+            browserName = "firefox";
+        }
+        return BrowsersFactory.valueOf(browserName).create();
     }
 
     @After
     public void tearDown(){
-        driver.quit();
+  //      driver.quit();
     }
 }
--- src/test/java/BrowsersFactory.java
+++ src/test/java/BrowsersFactory.java
@@ -2,22 +2,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public enum  BrowsersFactory {
     chrome {
         public WebDriver create() {
+            updateProperty("chrome");
             ChromeOptions options = new ChromeOptions();
-           // options.addArguments("--disable-notifications");
+            options.addArguments("--disable-notifications");
             return new ChromeDriver();
         }
     },
     firefox {
         public WebDriver create() {
+            updateProperty("firefox");
+            System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
+            System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "./firefox_logs");
+            FirefoxOptions options = new FirefoxOptions();
+            options.addPreference("dom.webnotifications.enabled", false);
             return new FirefoxDriver();
         }
     };
 
     public WebDriver create() {
         return null;
+    }
+
+    void updateProperty(String browserName) {
+        System.out.println(String.format("\nstarting %s-browser......", browserName));
+        if (System.getProperty("browser") == null) System.setProperty("browser", browserName);
     }
 }
